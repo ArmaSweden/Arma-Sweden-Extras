5651b525859fbe230b2e474f13d26926f7f2161c
Kentaro Hayashida <kentarohayashida@hotmail.com>
2021-12-09 14:29:15 +0100

    Improved vehicle config to allow for multiple variants of Strv 122

--- addons/ase_vehicles/config.cpp
+++ addons/ase_vehicles/config.cpp
@@ -18,15 +18,23 @@ class CfgFactionClasses
 	{
 		displayName = "Swedish Armed Forces";
 		author = "SageNTitled of Arma Sweden";
-		icon = "";
+		icon = ""; // TODO: Add icon
 		priority = 1;
 		side = 1;
 	};
 };
 
-class CfgEditorSubcategories
+class CfgVehicleClasses
 {
 	class ASE_B_Sweden_tanks
+	{
+		displayName = "Tanks";
+	};
+};
+
+class CfgEditorSubcategories
+{
+	class ASE_Eden_tanks
 	{
 		displayName = "Tanks";
 	};
@@ -35,15 +43,44 @@ class CfgEditorSubcategories
 class CfgVehicles
 {
 	class CUP_B_Leopard2A6_GER;
-	class ASE_B_Strv122 : CUP_B_Leopard2A6_GER
+	class CUP_B_Leopard2A6_GER_import : CUP_B_Leopard2A6_GER { class EventHandlers; };
+	class ASE_B_Strv122 : CUP_B_Leopard2A6_GER_import
+	{
+		scope = 0;
+		scopeCurator = 0;
+		vehicleClass = "ASE_B_Sweden_tanks";
+		class EventHandlers: EventHandlers
+		{
+			init = "if (local (_this select 0)) then { [(_this select 0), """", [], false] call BIS_fnc_initVehicle; };";
+		};
+		class textureSources
+		{
+			class M90_Woodland
+			{
+				displayName = "M90 Woodland";
+				author = "SageNTitled of Arma Sweden";
+				textures[] = {
+					"Arma-Sweden-Extras\addons\ase_vehicles\data\hull.jpg",
+					"Arma-Sweden-Extras\addons\ase_vehicles\data\turret.jpg",
+					"Arma-Sweden-Extras\addons\ase_vehicles\data\wheels.jpg"
+				};
+				factions[] = { 
+					"ASE_B_Sweden",
+					"CUP_B"
+				};
+			};
+		};
+	};
+	class ASE_B_Strv122_SWE : ASE_B_Strv122
 	{
 		scope = 2;
 		scopeCurator = 2;
-		displayName = "Strv 122";
+		displayName = "Strv 122 (Woodland)";
+		textureList[] = {
+			"M90_Woodland", 1
+		};
 		side = 1;
 		faction = "ASE_B_Sweden";
-		editorSubcategory = "ASE_B_Sweden_tanks";
-		hiddenSelectionsTextures[] = {"Arma-Sweden-Extras\addons\ase_vehicles\data\hull.jpg", "Arma-Sweden-Extras\addons\ase_vehicles\data\turret.jpg"};
-		enginePower = 100000; // TODO: Remove
+		editorSubcategory = "ASE_Eden_tanks";
 	};
 };
