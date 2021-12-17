ebd4dfa5b7145f4b607157482f3afa98fa98c1f6
Kentaro Hayashida <kentarohayashida@hotmail.com>
2021-12-17 01:10:04 +0100

    Class formatting

--- addons/ase_common/config.cpp
+++ addons/ase_common/config.cpp
@@ -1,5 +1,7 @@
-class CfgPatches {
-	class ASE_common {
+class CfgPatches
+{
+	class ASE_common
+	{
 		author = "SageNTitled of Arma Sweden";
 		name = "Arma 3 - User Interface";
 		url = "";
@@ -10,8 +12,10 @@ class CfgPatches {
 	};
 };
 
-class CfgMarkers {
-	class Flag {
+class CfgMarkers
+{
+	class Flag
+	{
 		name = "$STR_CFG_MARKERS_FLAG";
 		icon = "A3\ui_f\data\map\markers\military\circle_CA.paa";
 		color[] = {1, 0, 0, 1};
@@ -19,7 +23,8 @@ class CfgMarkers {
 		shadow = 1;
 		scope = 0;
 	};
-    	class ASE_flag_Sweden: Flag {
+    class ASE_flag_Sweden: Flag
+		{
 		name = "Sweden";
 		icon = "Arma-Sweden-Extras\addons\ASE_common\flag_sweden.paa";
 		texture = "Arma-Sweden-Extras\addons\ASE_common\flag_sweden.paa";
