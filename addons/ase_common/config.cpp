c5ca9f062443bf322def9f0eef12d016d650130e
Kentaro Hayashida <kentarohayashida@hotmail.com>
2021-12-20 12:26:51 +0100

    Removed flag marker

--- addons/ase_common/config.cpp
+++ addons/ase_common/config.cpp
@@ -11,27 +11,3 @@ class CfgPatches
 		weapons[] = {};
 	};
 };
-
-class CfgMarkers
-{
-	class Flag
-	{
-		name = "$STR_CFG_MARKERS_FLAG";
-		icon = "A3\ui_f\data\map\markers\military\circle_CA.paa";
-		color[] = {1, 0, 0, 1};
-		size = 32;
-		shadow = 1;
-		scope = 0;
-	};
-    class ASE_flag_Sweden: Flag
-		{
-		name = "Sweden";
-		icon = "Arma-Sweden-Extras\addons\ASE_common\flag_sweden.paa";
-		texture = "Arma-Sweden-Extras\addons\ASE_common\flag_sweden.paa";
-		size = 36;
-		scope = 1;
-		shadow = 0;
-		color[] = {1, 1, 1, 1};
-		markerClass = "Flags";
-	};
-};
