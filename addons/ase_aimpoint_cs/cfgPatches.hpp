b46a3d500676ee6050798c5b4e2cca5e321e3a66
Kentaro Hayashida <kentarohayashida@hotmail.com>
2022-07-08 15:40:27 +0200

    Code formatting

--- addons/ase_aimpoint_cs/cfgPatches.hpp
+++ addons/ase_aimpoint_cs/cfgPatches.hpp
@@ -5,9 +5,9 @@ class CfgPatches
 		author = "SageNTitled of Arma Sweden";
 		name = "ASE Aimpoint CS";
 		url = "https://www.armasweden.se/";
-		requiredAddons[]	 = {"A3_Data_F"};
-		requiredVersion		 = 0.1;
-		units[]				 = {};
-		weapons[]			 = {"ASE_optic_AimpointCS"};
+		requiredAddons[] = {"A3_Data_F"};
+		requiredVersion	= 0.1;
+		units[]	= {};
+		weapons[] = {"ASE_optic_AimpointCS"};
 	};
 };
